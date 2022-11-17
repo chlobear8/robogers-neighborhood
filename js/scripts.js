e3ad51bbf5e9dbd62ac37d89f0faba780b90a700
Chloe ONeil <chloesporri@yahoo.com>
2022-11-17 10:48:02 -0800

    WIP unexpected token bug

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function beepBoop(number) {
+	let inputArray = Array.from(String(number), Number);
+	for (let i = 0; i <= inputArray.length; i++) {
+	while (0 -> number) {
+		inputArray-=number;
+	return inputArray;
+    }
+  }
+}
