db0b8776e2590bde5032f33b7beaf278e190f72b
Chloe ONeil <chloesporri@yahoo.com>
2022-11-17 16:38:53 -0800

    add first output value for 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 function beepBoop(number) {
 	let outputArray = []; 
 	for (let i = 0; i <= number; i++) {
+    let digits=Array.from(String(i), Number) 
     outputArray[i]=i;
+    if (digits.includes(1)) {
+      outputArray[i]="Beep!"
+    }
   }
 	return outputArray;
 
